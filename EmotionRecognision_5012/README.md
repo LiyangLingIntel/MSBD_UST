6d405bcf714bb3001f4a31231c14b4c340b1fcf4
lyonling <935257580@qq.com>
2019-03-28 01:09:07 +0800

    rename folders

--- EmotionRecognision_5012/README.md
+++
@@ -1,4 +0,0 @@
-# Face Detection & Smile Recognition 
-*msbd5012 group project repository*
-
-The final result is we could use real time camera to detect the faces  and make classification on each face that's smiling or not.
--- PollutionPrediction_5002/src/evaluation.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def smape(actual, predicted):
-    dividend= np.abs(np.array(actual) - np.array(predicted))
-    c = np.array(actual) + np.array(predicted)
-    
-    return 2 * np.mean(np.divide(dividend, c, out=np.zeros_like(dividend), where=c!=0, casting='unsafe'))
